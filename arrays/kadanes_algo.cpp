fafe48caa629cd531dbb6f79a3734d6d8cbafd59
gowthamv441 <31705808+gowthamv441@users.noreply.github.com>
2018-07-07 22:00:04 -0700

    maximum sum contiguous subarray

---
+++ arrays/kadanes_algo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+main()
+{
+  int test_case;
+  cin>>test_case;
+  for(int t=1;t<=test_case;t++)
+  {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+      cin>>arr[i];
+
+    int max_so_for=arr[0];
+    int curr_max=arr[0];
+    int s=0,start,end;
+    for(int i=1;i<n;i++)
+    {
+      curr_max+=arr[i];
+      if(max_so_for<curr_max)
+      {
+        start=s;
+        end=i;
+        max_so_for=curr_max;
+      }
+      if(curr_max<0)
+      {
+        curr_max=0;
+        s=i+1;
+      }
+    }
+    cout<<"#case"<<t<<" "<<max_so_for<<" "<<start<<" "<<end<<endl;
+  }
+}
