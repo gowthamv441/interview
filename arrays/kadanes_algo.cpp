5c465d3be31968fc1df4608f40816ca9cf19935c
gowthamv441 <31705808+gowthamv441@users.noreply.github.com>
2018-07-15 04:46:51 -0700

    kadane with range

--- arrays/kadanes_algo.cpp
+++ arrays/kadanes_algo.cpp
@@ -1,35 +1,40 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-main()
-{
-  int test_case;
-  cin>>test_case;
-  for(int t=1;t<=test_case;t++)
-  {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-      cin>>arr[i];
 
-    int max_so_for=INT_MIN;
+int maximum_sum(int arr[],int n)
+{
     int curr_max=0;
-    int s=0,start,end;
+    int max_so_far=INT_MIN;
+    int start,end,s=0;
     for(int i=0;i<n;i++)
     {
-      curr_max+=arr[i];
-      if(max_so_for<curr_max)
-      {
-        start=s;
-        end=i;
-        max_so_for=curr_max;
-      }
-      if(curr_max<0)
-      {
-        curr_max=0;
-        s=i+1;
-      }
+        curr_max+=arr[i];
+        if(curr_max>max_so_far)
+        {
+            max_so_far=curr_max;
+            start=s;
+            end=i;
+        }
+        if(curr_max<0)
+        {
+            curr_max=0;
+            s=i+1;
+        }
     }
-    cout<<"#case"<<t<<" "<<max_so_for<<" "<<start<<" "<<end<<endl;
-  }
+    cout<<start<<" "<<end<<endl;
+    return max_so_far;
+}
+int main() {
+    int test_case;
+    cin>>test_case;
+    for(int t=1;t<=test_case;t++)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cout<<maximum_sum(arr,n)<<endl;
+    }
+	return 0;
 }
