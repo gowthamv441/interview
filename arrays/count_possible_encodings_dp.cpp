26b5c6cf252e87b85ef9998a53b1b164d50a021b
gowthamv441 <31705808+gowthamv441@users.noreply.github.com>
2018-07-17 06:05:26 -0700

    dp approach for encoding problem

---
+++ arrays/count_possible_encodings_dp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int count_encoding(string s)
+{
+  int count[s.length()+1];
+  count[0]=1;
+  count[1]=1;
+  for(int i=2;i<=s.length();i++)
+  {
+    count[i]=0;
+    if(s[i-1]>'0')
+      count[i]=count[i-1];
+    if (s[i-2] == '1' || (s[i-2] == '2' && s[i-1] < '7') )
+      count[i]+=count[i-2];
+  }
+  return count[s.length()];
+}
+main()
+{
+  string s;
+  cin>>s;
+  if(s[0]!='0')
+    cout<<count_encoding(s)<<endl;
+}
